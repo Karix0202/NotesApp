42bcfc374eda9270022b3e096430de3a56aab52a
Karix0202 <kari.klucz@gmail.com>
2022-06-30 13:49:30 +0200

    added FolderType and Add view

--- src/Controller/FolderController.php
+++ src/Controller/FolderController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Folder;
+use App\Form\FolderType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/folder', name: 'folder_')]
 class FolderController extends AbstractController
 {
-    #[Route('/folder', name: 'app_folder')]
-    public function index(): Response
+    #[Route('/add', name: 'add')]
+    public function add(): Response
     {
-        return $this->render('folder/index.html.twig', [
-            'controller_name' => 'FolderController',
+        $folder = new Folder();
+        $form = $this->createForm(FolderType::class, $folder);
+
+        return $this->render('folder/add.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
